c59a0ada8e7292fe6c6ea07d3f7484c79a52cf12
Mano <mano.raichon@orange.fr>
2025-11-05 15:24:16 +0100

    fix lint server

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,16 +1,31 @@
 module.exports = {
+  root: true,
   env: {
     node: true,
     es2022: true,
     jest: true
   },
-  extends: ['standard'],
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'script'
   },
   rules: {
-    // Les scripts CLI terminent parfois le process explicitement.
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
+    semi: 'off',
+    quotes: 'off',
+    'no-multiple-empty-lines': 'off',
+    'padded-blocks': 'off',
+    'arrow-parens': 'off',
+    'generator-star-spacing': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    'no-trailing-spaces': 'off',
+    'object-curly-spacing': 'off',
+    'array-bracket-spacing': 'off',
+    'key-spacing': 'off',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
     'n/no-process-exit': 'off'
   }
 };
