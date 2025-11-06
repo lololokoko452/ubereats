5fbc1e56868a7bcd5f39a6c22fbc0e07206036c4
Mano <mano.raichon@orange.fr>
2025-11-06 15:02:29 +0100

    fix app sentry

--- server/src/app.js
+++ server/src/app.js
@@ -13,16 +13,12 @@ const routes = require('./routes')
 
 const app = express()
 
-app.use(Sentry.Handlers.requestHandler())
-app.use(Sentry.Handlers.tracingHandler())
-
 app.use(cors())
 app.use(express.json({ limit: '1mb' }))
 app.use(morgan('dev'))
 
 const staticDir = path.join(__dirname, '..', 'public')
 if (fs.existsSync(staticDir)) {
-  // Serve built frontend assets when present (production Docker image)
   app.use(express.static(staticDir))
 }
 
@@ -40,7 +36,7 @@ app.use((req, res, next) => {
   next(createError(404, `No route found for ${req.method} ${req.originalUrl}`))
 })
 
-app.use(Sentry.Handlers.errorHandler())
+app.use(Sentry.expressErrorHandler())
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
@@ -49,7 +45,6 @@ app.use((err, req, res, next) => {
     err.message || 'Une erreur est survenue, veuillez réessayer plus tard.'
 
   if (status >= 500) {
-    // eslint-disable-next-line no-console
     console.error(err)
   }
 
